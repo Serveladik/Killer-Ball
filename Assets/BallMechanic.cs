9076ab2f7ee09f11b4965a3a03d2c3c0779818b2
Vladyslav <serveladik@gmail.com>
2020-09-14 12:57:29 +0300

    all

---
+++ Assets/BallMechanic.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMechanic : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public Transform ballSpawn;
+    public float scaleTime = 10f;
+    public Vector3 minScale = new Vector3();
+    public GameObject ballClone;
+    //public float minScaleSize;
+    
+
+    void Update()
+    {
+        Shoot();
+
+    }
+
+    void Shoot()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(transform.localScale.magnitude>=minScale.magnitude)
+            {
+                ballClone = Instantiate(ballPrefab,ballSpawn.transform.position,Quaternion.identity) as GameObject;
+            }
+        }
+        if(Input.GetMouseButton(0))
+        {
+            if(transform.localScale.magnitude>minScale.magnitude)
+            {
+                transform.localScale-=Vector3.one * scaleTime/40 * Time.deltaTime;
+                ballClone.transform.localScale+=Vector3.one * scaleTime/40 * Time.deltaTime;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+    }
+
+
+
+}
